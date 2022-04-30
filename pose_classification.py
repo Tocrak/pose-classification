62aed2365311e6c9db94b22dd273abe216c042f4
Tocrak <virgiliustsirdea@yandex.ru>
2022-05-01 01:08:03 +0200

    add more model evaluation metrics

--- pose_classification.py
+++ pose_classification.py
@@ -23,6 +23,10 @@ def train_test_models(models):
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         print("Accuracy:", accuracy_score(y_test, y_pred))
+        print("Recall:", recall_score(y_test, y_pred))
+        print("Precision:", precision_score(y_test, y_pred))
+        print("F-score:", f1_score(y_test, y_pred))
+        print("Confusion matrix:\n", confusion_matrix(y_test, y_pred), "\n")
 
 
 op_model = OpenposeRunner()
